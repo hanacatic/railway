c2fae39472a9e1e55880a98e8d9ea0eba65cc361
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-13 13:25:50 +0100

    JourneyDao javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/JourneyDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/JourneyDao.java
@@ -5,12 +5,53 @@ import ba.unsa.etf.rpr.domain.RailwayStation;
 import ba.unsa.etf.rpr.domain.Train;
 
 import java.sql.Time;
+import java.util.Date;
 import java.util.List;
-
+/**
+ * Dao interface for journey domain bean
+ * @author Hana Catic
+ * */
 public interface JourneyDao extends Dao{
+    /**
+     * Finds all journeys made by a given train
+     * @param train which to look for
+     * @return list of journeys
+     * */
     List<Journey> searchByTrain(Train train);
+    /**
+     * Finds all journeys from a given railway station
+     * @param departureStation station in which to look for departing trains
+     * @return list of journeys
+     * */
     List<Journey> searchByDepartureStation(RailwayStation departureStation);
+    /**
+     * Finds all journeys  a given railway station
+     * @param arrivalStation station in which to look for arriving trains
+     * @return list of journeys
+     * */
     List<Journey> searchByArrivalStation(RailwayStation arrivalStation);
+    /**
+     * Finds all trains leaving on a given date, all journeys starting on a given date
+     * @param departureDate date on which to look for departing trains, journeys starting
+     * @return list of journeys
+     * */
+    List<Journey> searchByDepartureTime(Date departureDate);
+    /**
+     * Finds all trains arriving on a given date, all journeys ending on a given date
+     * @param arrivalDate date on which to look for arriving trains, journeys ending
+     * @return list of journeys
+     * */
+    List<Journey> searchByArrivalTime(Date arrivalDate);
+    /**
+     * Finds all trains leaving at a given time, all journeys starting at a given time
+     * @param departureTime time when to look for departing trains, journeys starting
+     * @return list of journeys
+     * */
     List<Journey> searchByDepartureTime(Time departureTime);
+    /**
+     * Finds all trains arriving at a given time, all journeys ending at a given time
+     * @param arrivalTime time when to look for arriving trains, journeys ending
+     * @return list of journeys
+     * */
     List<Journey> searchByArrivalTime(Time arrivalTime);
 }
