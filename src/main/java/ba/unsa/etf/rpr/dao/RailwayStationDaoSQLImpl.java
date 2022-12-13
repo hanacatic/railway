727d295a68b0f6bc7f4e7394a58a3d2002b2e37c
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-13 13:33:52 +0100

    RailwayStationDaoSQLImpl getById

--- src/main/java/ba/unsa/etf/rpr/dao/RailwayStationDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RailwayStationDaoSQLImpl.java
@@ -2,9 +2,22 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.RailwayStation;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
 
 public class RailwayStationDaoSQLImpl implements RailwayStationDao{
+    private Connection connection;
+
+    public RailwayStationDaoSQLImpl(){
+        try{
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
     @Override
     public Object add(Object item) {
         return null;
@@ -17,6 +30,20 @@ public class RailwayStationDaoSQLImpl implements RailwayStationDao{
 
     @Override
     public Object getById(int id) {
+        try{
+            String query = "SELECT * FROM RailwayStations WHERE id = ?";
+            PreparedStatement stmt = this.connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()){
+                RailwayStation station = new RailwayStation();
+                station.setId(id);
+                station.setName(rs.getString("name"));
+                return station;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 
