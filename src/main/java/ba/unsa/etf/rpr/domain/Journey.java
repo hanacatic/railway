4733db7582ce38236b258afb8f36f747b7de74ac
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-13 12:55:19 +0100

    Journey

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Journey.java
@@ -0,0 +1,107 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.Objects;
+
+public class Journey {
+    private int id;
+    private int trainId;
+    private int departureStationId;
+    private int arrivalStationId;
+    private Date departureDate;
+    private Date arrivalDate;
+    private Time departureTime;
+    private Time arrivalTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTrainId() {
+        return trainId;
+    }
+
+    public void setTrainId(int trainId) {
+        this.trainId = trainId;
+    }
+
+    public int getDepartureStationId() {
+        return departureStationId;
+    }
+
+    public void setDepartureStationId(int departureStationId) {
+        this.departureStationId = departureStationId;
+    }
+
+    public int getArrivalStationId() {
+        return arrivalStationId;
+    }
+
+    public void setArrivalStationId(int arrivalStationId) {
+        this.arrivalStationId = arrivalStationId;
+    }
+
+    public Date getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(Date departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public Date getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(Date arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public Time getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Time departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Time getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(Time arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Journey{" +
+                "id=" + id +
+                ", trainId=" + trainId +
+                ", departureStationId=" + departureStationId +
+                ", arrivalStationId=" + arrivalStationId +
+                ", departureDate=" + departureDate +
+                ", arrivalDate=" + arrivalDate +
+                ", departureTime=" + departureTime +
+                ", arrivalTime=" + arrivalTime +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Journey journey = (Journey) o;
+        return id == journey.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, trainId, departureStationId, arrivalStationId, departureDate, arrivalDate, departureTime, arrivalTime);
+    }
+}
---
+++ src/main/java/ba/unsa/etf/rpr/domain/Train.java
@@ -0,0 +1,56 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Train {
+    private int id;
+    private String name;
+    private Date dateBought;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateBought() {
+        return dateBought;
+    }
+
+    public void setDateBought(Date dateBought) {
+        this.dateBought = dateBought;
+    }
+
+    @Override
+    public String toString() {
+        return "Train{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dateBought=" + dateBought +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Train train = (Train) o;
+        return id == train.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, dateBought);
+    }
+}
