a80e579547e05a84de2a907ab59fb9bb09ab2e5b
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-13 13:07:03 +0100

    RailwayStationDao javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/RailwayStationDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RailwayStationDao.java
@@ -1,5 +1,16 @@
 package ba.unsa.etf.rpr.dao;
 
-public interface RailwayStationDao extends Dao{
+import ba.unsa.etf.rpr.domain.RailwayStation;
 
+/**
+ * Dao interface for railway station domain bean
+ * @author Hana Catic
+ * */
+public interface RailwayStationDao extends Dao{
+    /**
+     * Find a railway station by name
+     * @param name of railway station to look for in database
+     * @return railway stations of given name
+     * */
+    RailwayStation searchByName(String name);
 }
