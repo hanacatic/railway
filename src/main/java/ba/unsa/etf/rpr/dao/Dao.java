c3277f05f0f74aa0ed88843afa5f975f74f6b672
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-13 12:35:51 +0100

    Dao javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,11 +1,37 @@
 package ba.unsa.etf.rpr.dao;
 
 import java.util.List;
-
+/**
+ * Generic root Dao interface for all classes
+ * @author Hana Catic
+ * */
 public interface Dao <Type>{
-    Type add(Type item);
+    /**
+     * Saves entity in database
+     * @param item to be saved in the database
+     * @return saved item
+     * */
+    Type add(Type item);/**
+     * Updates entity in database based on id
+     * @param item entity to be updated
+     * @return updated item
+     * */
+
     Type update(Type item);
+    /**
+     * Finds entity in database with given id
+     * @param id primary key of entity
+     * @return database entity
+     * */
     Type getById(int id);
+    /**
+     * Hard delete of entity
+     * @param id primary key of entity to be deleted from database
+     * */
     void delete(int id);
+    /**
+     * Gets list of all entities from database
+     * @return List of all entities form database
+     * */
     List<Type> getAll();
 }
