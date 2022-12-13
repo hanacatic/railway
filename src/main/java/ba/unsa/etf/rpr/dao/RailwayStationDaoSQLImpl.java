dd8c3fe8cf9dfde3352d08423355dcfe1e7ab679
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-13 13:26:53 +0100

    RailwayStationDaoSQLImpl

---
+++ src/main/java/ba/unsa/etf/rpr/dao/RailwayStationDaoSQLImpl.java
@@ -0,0 +1,37 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.RailwayStation;
+
+import java.util.List;
+
+public class RailwayStationDaoSQLImpl implements RailwayStationDao{
+    @Override
+    public Object add(Object item) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object item) {
+        return null;
+    }
+
+    @Override
+    public Object getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List getAll() {
+        return null;
+    }
+
+    @Override
+    public RailwayStation searchByName(String name) {
+        return null;
+    }
+}
