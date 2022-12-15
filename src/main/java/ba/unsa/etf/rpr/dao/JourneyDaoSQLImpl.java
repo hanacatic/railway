dc13362b5da1ca055e9346f0dd5a0d676e5e9e4c
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-15 17:52:19 +0100

    JourneyDaoSQLImpl searchByDepartureStation

--- src/main/java/ba/unsa/etf/rpr/dao/JourneyDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/JourneyDaoSQLImpl.java
@@ -142,7 +142,28 @@ public class JourneyDaoSQLImpl implements JourneyDao{
 
     @Override
     public List<Journey> searchByDepartureStation(RailwayStation departureStation) {
-        return null;
+        List<Journey> journeys = new ArrayList<Journey>();
+        try{
+            String query = "SELECT * FROM Journeys WHERE departureStation = ?";
+            PreparedStatement stmt = this.connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            stmt.setInt(1, departureStation.getId());
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                Journey journey = new Journey();
+                journey.setId(rs.getInt("id"));
+                journey.setTrainId(rs.getInt("train"));
+                journey.setDepartureStationId(rs.getInt("departureStation"));
+                journey.setArrivalStationId(rs.getInt("arrivalStation"));
+                journey.setDepartureDate(rs.getDate("departureDate"));
+                journey.setArrivalDate(rs.getDate("arrivalDate"));
+                journey.setDepartureTime(rs.getTime("departureTime"));
+                journey.setArrivalTime(rs.getTime("arrivalTime"));
+                journeys.add(journey);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return journeys;
     }
 
     @Override
