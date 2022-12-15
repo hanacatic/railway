f51ab8af85b95d0a861e14618d1ca2a20131e624
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-15 17:31:07 +0100

    JourneyDaoSQLImpl getAll

--- src/main/java/ba/unsa/etf/rpr/dao/JourneyDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/JourneyDaoSQLImpl.java
@@ -5,6 +5,7 @@ import ba.unsa.etf.rpr.domain.RailwayStation;
 import ba.unsa.etf.rpr.domain.Train;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -61,7 +62,27 @@ public class JourneyDaoSQLImpl implements JourneyDao{
 
     @Override
     public List<Journey> getAll() {
-        return null;
+        List<Journey> journeys = new ArrayList<Journey>();
+        try{
+            String query = "SELECT * FROM Journeys";
+            PreparedStatement stmt = this.connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                Journey journey = new Journey();
+                journey.setId(rs.getInt("id"));
+                journey.setTrainId(rs.getInt("train"));
+                journey.setDepartureStationId(rs.getInt("departureStation"));
+                journey.setArrivalStationId(rs.getInt("arrivalStation"));
+                journey.setDepartureDate(rs.getDate("departureDate"));
+                journey.setArrivalDate(rs.getDate("arrivalDate"));
+                journey.setDepartureTime(rs.getTime("departureTime"));
+                journey.setArrivalTime(rs.getTime("arrivalTime"));
+                journeys.add(journey);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return journeys;
     }
 
     @Override
