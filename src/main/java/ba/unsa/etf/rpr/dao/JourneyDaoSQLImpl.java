8b559ad8e4b74d0e74c09c36c63f58f7ce720e51
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-15 17:33:51 +0100

    JourneyDaoSQLImpl delete

--- src/main/java/ba/unsa/etf/rpr/dao/JourneyDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/JourneyDaoSQLImpl.java
@@ -57,6 +57,14 @@ public class JourneyDaoSQLImpl implements JourneyDao{
 
     @Override
     public void delete(int id) {
+        try{
+            String insert = "DELETE FROM Journeys WHERE id = ?";
+            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
     }
 
