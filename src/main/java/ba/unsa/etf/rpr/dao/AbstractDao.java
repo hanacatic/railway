bf61e21c30e82d7dcfca43aad8ee0b8b4c70acc9
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-22 17:18:14 +0100

    AbstractDao

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -2,8 +2,20 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Idable;
 import java.sql.*;
+import java.util.*;
 
 public abstract class AbstractDao<Type extends Idable> implements Dao<Type>{
     private Connection connection;
     private String tableName;
+
+    public AbstractDao(String tableName){
+
+    }
+
+    public Connection getConnection(){ return null;}
+    public Type getById(int id){return null;}
+    public List<Type> getAll() {return null;}
+    public Type add(Type item){return null;}
+    public Type update(Type item){return null;}
+
 }
