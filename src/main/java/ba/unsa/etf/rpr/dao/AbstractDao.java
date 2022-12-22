b5c33a0f19844c3f8aea44b340f7a7f8558c77d2
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-22 17:25:01 +0100

    AbstractDao constructor

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -9,7 +9,18 @@ public abstract class AbstractDao<Type extends Idable> implements Dao<Type>{
     private String tableName;
 
     public AbstractDao(String tableName){
-
+        try{
+            this.tableName = tableName;
+            Properties p = new Properties();
+            p.load(ClassLoader.getSystemResource("application.properties").openStream());
+            String url = p.getProperty("db.connection_string");
+            String username = p.getProperty("db.username");
+            String password = p.getProperty("db.password");
+            this.connection = DriverManager.getConnection(url, username, password);
+        }catch(Exception e){
+            System.out.println("Connection to database failed.");
+            e.printStackTrace();
+        }
     }
 
     public Connection getConnection(){ return this.connection;}
