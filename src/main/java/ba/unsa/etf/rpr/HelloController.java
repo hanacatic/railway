922f9facdd3f196255b562673d3f69e8738aba9f
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-29 14:42:48 +0100

    gui setup

---
+++ src/main/java/ba/unsa/etf/rpr/HelloController.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+
+public class HelloController {
+    public void buttonClick(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/ba/unsa/etf/rpr/gui.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class gui extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/glavniProzor.fxml"));
+        Scene scene = new Scene((Parent) fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+        stage.setTitle("Prijava!");
+        stage.setScene(scene);
+        stage.setMinHeight(90);
+        stage.setMinWidth(120);
+        //stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
