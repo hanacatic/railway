cc474b63e43bdb948fd3b1483aba9e058548b6ca
Hana Catic <hcatic1@etf.unsa.ba>
2022-12-29 17:17:10 +0100

    RailwayStation domain update

--- src/main/java/ba/unsa/etf/rpr/domain/RailwayStation.java
+++ src/main/java/ba/unsa/etf/rpr/domain/RailwayStation.java
@@ -7,6 +7,9 @@ import java.util.Objects;
 public class RailwayStation implements Idable {
     private int id;
     private String name;
+    private String adress;
+    private String city;
+    private String country;
 
     public int getId() {
         return id;
@@ -24,11 +27,38 @@ public class RailwayStation implements Idable {
         this.name = name;
     }
 
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     @Override
     public String toString() {
         return "RailwayStation{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", adress='" + adress + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
                 '}';
     }
 
@@ -42,6 +72,6 @@ public class RailwayStation implements Idable {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(id, name, adress, city, country);
     }
 }
