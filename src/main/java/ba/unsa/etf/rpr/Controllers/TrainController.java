3f0be1c6aad4e57bac7014732dda6f666d99e774
Hana Catic <hcatic1@etf.unsa.ba>
2023-01-09 13:43:52 +0100

    refreshTrains

--- src/main/java/ba/unsa/etf/rpr/Controllers/TrainController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/TrainController.java
@@ -1,11 +1,10 @@
 package ba.unsa.etf.rpr.Controllers;
 
 import ba.unsa.etf.rpr.Bussiness.TrainManager;
+import ba.unsa.etf.rpr.Exceptions.RailwayException;
 import ba.unsa.etf.rpr.domain.Train;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.collections.FXCollections;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.sql.Date;
@@ -15,11 +14,24 @@ public class TrainController {
 
     public TextField trainName;
     public DatePicker trainDateBought;
+    public TableView trainsTable;
+    public TableColumn<Train, String> idColumn;
+    public TableColumn<Train, String> nameColumn;
+    public TableColumn<Train, Date> dateBoughtColumn;
 
 
-    /*public void initialize(){
-        /*idColumn.setCellValueFactory(new PropertyValueFactory<Train, String>("id"));
+    public void initialize(){
+        idColumn.setCellValueFactory(new PropertyValueFactory<Train, String>("id"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<Train, String>("name"));
         dateBoughtColumn.setCellValueFactory(new PropertyValueFactory<Train, Date>("date bought"));
-    }*/
+    }
+
+    private void refreshTrains(){
+        try{
+            trainsTable.setItems(FXCollections.observableList(trainManager.getAll()));
+            trainsTable.refresh();
+        } catch (RailwayException e) {
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
 }
