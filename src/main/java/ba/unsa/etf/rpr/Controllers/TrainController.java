7806c14f80cc40b4859b77f2e3e757a4c8128f9a
Hana Catic <hcatic1@etf.unsa.ba>
2023-01-10 13:22:12 +0100

    trainController add

--- src/main/java/ba/unsa/etf/rpr/Controllers/TrainController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/TrainController.java
@@ -4,6 +4,7 @@ import ba.unsa.etf.rpr.Bussiness.TrainManager;
 import ba.unsa.etf.rpr.Exceptions.RailwayException;
 import ba.unsa.etf.rpr.domain.Train;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
@@ -23,7 +24,8 @@ public class TrainController {
     public void initialize(){
         idColumn.setCellValueFactory(new PropertyValueFactory<Train, String>("id"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<Train, String>("name"));
-        dateBoughtColumn.setCellValueFactory(new PropertyValueFactory<Train, Date>("date bought"));
+        dateBoughtColumn.setCellValueFactory(new PropertyValueFactory<Train, Date>("dateBought"));
+        refreshTrains();
     }
 
     private void refreshTrains(){
@@ -32,6 +34,18 @@ public class TrainController {
             trainsTable.refresh();
         } catch (RailwayException e) {
             new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
+
+    public void addTrain(ActionEvent actionEvent) {
+        try{
+            Train train = new Train();
+            train.setName(trainName.getText());
+            train.setDateBought(Date.valueOf(trainDateBought.getValue()));
+            train = trainManager.add(train);
+            refreshTrains();
+        } catch (RailwayException e) {
+            throw new RuntimeException(e.getMessage());//new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
         }
     }
 }
