3aaebd10cc794be1168b3605b0e0e689bcd7096d
Hana Catic <hcatic1@etf.unsa.ba>
2023-01-11 16:15:00 +0100

    railwayStation.fxml ui design

--- src/main/java/ba/unsa/etf/rpr/Controllers/RailwayStationController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/RailwayStationController.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class RailwayStationController {
+    public TextField stationName;
+    public TextField stationAdress;
+    public ComboBox stationCity;
+    public ComboBox stationCountry;
+    public TableView stationsTable;
+    public TableColumn idColumn;
+    public TableColumn nameColumn;
+    public TableColumn adressColumn;
+    public TableColumn cityColumn;
+    public TableColumn countryColumn;
+
+
+
+    public void addStation(ActionEvent actionEvent) {
+    }
+
+    public void updateStation(ActionEvent actionEvent) {
+    }
+
+    public void deleteStation(ActionEvent actionEvent) {
+    }
 }
