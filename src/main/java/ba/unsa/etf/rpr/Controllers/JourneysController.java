17d11881fe88ed851d2872897ec04ca800dbbad0
Hana Catic <hcatic1@etf.unsa.ba>
2023-01-17 00:31:37 +0100

    redesign

--- src/main/java/ba/unsa/etf/rpr/Controllers/JourneysController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/JourneysController.java
@@ -1,18 +1,27 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import ba.unsa.etf.rpr.App;
 import ba.unsa.etf.rpr.Bussiness.JourneyManager;
 import ba.unsa.etf.rpr.Exceptions.RailwayException;
 import ba.unsa.etf.rpr.domain.Journey;
 import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.Date;
 import java.sql.Time;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class JourneysController {
     private final JourneyManager journeyManager = new JourneyManager();
@@ -44,5 +53,24 @@ public class JourneysController {
         } catch(RailwayException e){
             new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
         }
+    }
+
+    public void addJourney(ActionEvent actionEvent) {
+    }
+
+    public void editTrains(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/train.fxml"));
+        Scene scene = new Scene((Parent) fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void editRailwayStations(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/railwayStation.fxml"));
+        Scene scene = new Scene((Parent) fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+        stage.setScene(scene);
+        stage.show();
     }
 }
