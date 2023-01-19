1c5cc6d4f4e2647cffc00695fd8b7bfa7def0718
Hana Catic <hcatic1@etf.unsa.ba>
2023-01-19 20:23:26 +0100

    Journey form

---
+++ src/main/java/ba/unsa/etf/rpr/Controllers/JourneyFormController.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr.Controllers;
+
+import javafx.event.ActionEvent;
+
+public class JourneyFormController {
+    public void addJourney(ActionEvent actionEvent) {
+    }
+
+    public void updateJourney(ActionEvent actionEvent) {
+    }
+
+    public void deleteJourney(ActionEvent actionEvent) {
+    }
+}
