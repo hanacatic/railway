4c4e1738e70ce92d982062be71b92235c22a4959
Hana Catic <hcatic1@etf.unsa.ba>
2023-01-21 20:26:33 +0100

    journeyForm fxid

--- src/main/java/ba/unsa/etf/rpr/Controllers/JourneyFormController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/JourneyFormController.java
@@ -1,8 +1,21 @@
 package ba.unsa.etf.rpr.Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
 
 public class JourneyFormController {
+    public ComboBox trainName;
+    public ComboBox departureStation;
+    public ComboBox arrivalStation;
+    public DatePicker departureDate;
+    public DatePicker arrivalDate;
+    public Spinner departureTimeH;
+    public Spinner departureTimeMin;
+    public Spinner arrivalTimeH;
+    public Spinner arrivalTimeMin;
+
     public void addJourney(ActionEvent actionEvent) {
     }
 
