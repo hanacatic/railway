0a461d8ba358c32d46d3850d8ec5ea42638b9d44
Hana Catic <hcatic1@etf.unsa.ba>
2023-02-14 22:52:06 +0100

    LoginController javaDoc

--- src/main/java/ba/unsa/etf/rpr/Controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/LoginController.java
@@ -7,14 +7,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
-
+/**
+ * Controller for managing Login
+ * @author Hana Catic
+ * */
 public class LoginController {
+
+    //components
     public Button loginBtn;
 
+    /**
+     * Opens admin stage that allows overview of all journeys, trains and railway stations and allows editing
+     * */
     public void actionLogin(ActionEvent actionEvent) throws IOException {
         Stage stage = new Stage();
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/journeys.fxml"));
