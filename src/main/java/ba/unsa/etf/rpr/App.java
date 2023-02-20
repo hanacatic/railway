aa3ac8efa3eb48fc1743db637ddfa7b75fb18608
Hana Catic <hcatic1@etf.unsa.ba>
2023-02-20 20:35:13 +0100

    beta cli

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,16 +1,33 @@
 package ba.unsa.etf.rpr;
 
-import ba.unsa.etf.rpr.dao.Dao;
-import ba.unsa.etf.rpr.dao.DaoFactory;
-import ba.unsa.etf.rpr.dao.RailwayStationDaoSQLImpl;
-import ba.unsa.etf.rpr.domain.RailwayStation;
-
+import org.apache.commons.cli.*;
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ) {
+    private static final Option addTrain = new Option("t", "add-train", false, "Adding new train to Railway database");
+    private static final Option addRailwayStation = new Option("r", "add-station", false, "Adding new railway station to Railway database");
+    private static final Option addJourney = new Option("j", "add-journey", false, "Adding new journey to Railway database");
+
+    private static final Option getTrains = new Option("getT", "get-train", false, "Printing all trains from Railway database");
+    private static final Option getRailwayStations = new Option("getR", "get-station", false, "Printing all railway stations from Railway database");
+    private static final Option getJourneys = new Option("getJ", "get-journey", false, "Printing all journey from Railway database");
+
+    public static Options addOptions(){
+        Options options = new Options();
+        options.addOption(addTrain);
+        options.addOption(addRailwayStation);
+        options.addOption(addJourney);
+        options.addOption(getTrains);
+        options.addOption(getRailwayStations);
+        options.addOption(getJourneys);
+        return options;
+    }
+    public static void main( String[] args ) throws Exception {
+        Options options = addOptions();
+        CommandLineParser commandLineParser = new DefaultParser();
+        CommandLine c = commandLineParser.parse(options, args);
 
     }
 }
