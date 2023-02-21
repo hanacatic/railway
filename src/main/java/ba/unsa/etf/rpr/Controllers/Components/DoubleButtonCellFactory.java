d40f8162253f1dca0c9fcc6cd0f93ef0f5f208a2
Hana Catic <hcatic1@etf.unsa.ba>
2023-02-21 13:48:23 +0100

    components javaDoc

--- src/main/java/ba/unsa/etf/rpr/Controllers/Components/DoubleButtonCellFactory.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/Components/DoubleButtonCellFactory.java
@@ -5,11 +5,18 @@ import javafx.event.EventHandler;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.util.Callback;
-
+/**
+ * Double button cell factory for creation of buttons for each cell in the table
+ * @param <Type>
+ * */
 public class DoubleButtonCellFactory<Type> implements Callback<TableColumn<Type, Type>, TableCell<Type, Type>> {
     private final EventHandler<ActionEvent> buttonOne;
     private final EventHandler<ActionEvent> buttonTwo;
-
+    /**
+     * Creates buttons edit and delete respectively for two given event handlers
+     * @param buttonOne event handler for first button - Edit
+     * @param buttonTwo event handler for second button - Delete
+     * */
     public DoubleButtonCellFactory(EventHandler<ActionEvent> buttonOne, EventHandler<ActionEvent> buttonTwo) {
         this.buttonOne = buttonOne;
         this.buttonTwo = buttonTwo;
--- src/main/java/ba/unsa/etf/rpr/Controllers/Components/DoubleButtonTableCell.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/Components/DoubleButtonTableCell.java
@@ -6,10 +6,18 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.layout.HBox;
-
+/**
+ * Custom component for rendering table cell with two buttons (Edit and Delete)
+ * @param <Type>  Bean class represented in the table cells
+ * */
 public class DoubleButtonTableCell<Type> extends TableCell<Type, Type> {
     private Button edit;
     private Button delete;
+    /**
+     * Default constructor
+     * @param buttonOne event handler for action on first button - Edit
+     * @param buttonTwo event handler for action on second button - Delete
+     * */
     public DoubleButtonTableCell(EventHandler<ActionEvent> buttonOne, EventHandler<ActionEvent> buttonTwo) {
         edit = new Button("Edit");
         edit.setOnAction(buttonOne);
